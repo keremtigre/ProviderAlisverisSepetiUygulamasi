a1e082d4a97374088886ed67a9d0e127e43e982f
Kerem <keremtigre@hotmail.com>
2022-01-16 15:48:16 +0300

    Yeni Ürünler Eklendi

--- lib/state/product_state.dart
+++ lib/state/product_state.dart
@@ -4,7 +4,13 @@ import 'package:flutter_provider_deneme_1/models/product.dart';
 class ProductState with ChangeNotifier {
   List<Product> _productList = [
     Product("Macbook air", "14000", "silver", "assets/mair.jpg", 0, 0),
-    Product("Macbook Pro", "17000", "rose", "assets/mpro.jpg", 0, 0)
+    Product("Macbook Pro", "17000", "rose", "assets/mpro.jpg", 0, 0),
+    Product("Macbook Pro", "17000", "rose", "assets/mpro.jpg", 0, 0),
+    Product("Macbook air", "14000", "silver", "assets/mair.jpg", 0, 0),
+    Product("Macbook Pro", "17000", "rose", "assets/mpro.jpg", 0, 0),
+    Product("Macbook air", "14000", "silver", "assets/mair.jpg", 0, 0),
+    Product("Macbook Pro", "17000", "rose", "assets/mpro.jpg", 0, 0),
+    Product("Macbook air", "14000", "silver", "assets/mair.jpg", 0, 0),
   ];
 
   List<Product> get product => _productList;
